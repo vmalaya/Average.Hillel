98cb13ae0af898ae2263273c08bdce9c3e38cef1
Valentina Malaya <valentinkamalaya@gmail.com>
2017-05-16 20:55:41 +0300

    first commit

---
+++ src/Average.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by Валентина on 16.05.2017.
+ */
+public class Average {
+    public static void main(String[] arg) {
+
+       System.out.println("Pleas input first number: ");
+        Scanner scanner = new Scanner(System.in);
+        double first = scanner.nextDouble();
+        System.out.println("Pleas input second number: ");
+        Scanner in = new Scanner(System.in);
+        double second = in.nextDouble();
+        double sum = first+second;
+        System.out.println(sum);
+        System.out.println("Average:" + (sum) / 2);
+
+
+    }
+}
+
